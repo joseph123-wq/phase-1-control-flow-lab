ab1f7d4b31179dd8bd80fa2e32e5dce359402459
JosephKagwi <josephkagwi28@gmail.com>
2023-09-18 21:28:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet) {
+  if (distanceInFeet <= 400) {
+    return 'This one is on me!';
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distanceInFeet > 2500) {
+    return 'No can do.';
+  }
+}
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
